185baf8cbcebd8e25a65c9f585e08f2383135a21
Imagine <imaginejhy@163.com>
2023-12-05 18:14:56 +0800

    update

--- include/Imagine_MapReduce/MapTaskService.h
+++ include/Imagine_MapReduce/MapTaskService.h
@@ -89,6 +89,7 @@ Imagine_Rpc::Status MapTaskService<reader_key, reader_value, key, value>::MapTas
                 Internal::HeartBeatResponseMessage response_msg;
                 heartbeat_stub->ConnectServer();
                 MapReduceUtil::GenerateHeartBeatStartMessage(&heartbeat_request_msg, Internal::Identity::Mapper, runner->GetFileName(), runner->GetId());
+                LOG_INFO("Mappper Task Start, split id is %d", reader->GetSplitId());
                 heartbeat_stub->CallConnectServer(&heartbeat_request_msg, &response_msg);
                 if (response_msg.status_() == Internal::Status::Ok) {
                     timerid = runner->GetRpcServer()->SetTimer(std::bind(runner->GetTimerCallback(), heartbeat_stub, reader), DEFAULT_HEARTBEAT_INTERVAL_TIME, DEFAULT_HEARTBEAT_DELAY_TIME);
